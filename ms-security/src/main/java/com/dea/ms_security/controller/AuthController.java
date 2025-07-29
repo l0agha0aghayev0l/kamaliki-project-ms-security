3da39207d8311b7760c31aee6fdf5c9a9c4a5a89
l0agha0aghayev0l <l.agha.aghayev.l@gmail.com>
2025-07-29 20:02:37 +0400

    main

--- ms-security/src/main/java/com/dea/ms_security/controller/AuthController.java
+++ ms-security/src/main/java/com/dea/ms_security/controller/AuthController.java
@@ -5,7 +5,6 @@ import java.util.Set;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dea.ms_security.request.LoginRequest;
@@ -15,7 +14,6 @@ import com.dea.ms_security.service.AuthService;
 import lombok.RequiredArgsConstructor;
 
 @RestController
-@RequestMapping("/auth")
 @RequiredArgsConstructor
 public class AuthController {    
     //login
--- ms-security/src/main/java/com/dea/ms_security/controller/TokenController.java
+++ ms-security/src/main/java/com/dea/ms_security/controller/TokenController.java
@@ -1,10 +1,8 @@
 package com.dea.ms_security.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/token")
 public class TokenController {
     
 }
